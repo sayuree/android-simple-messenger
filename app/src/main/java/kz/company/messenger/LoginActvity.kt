45b0e0d621a3f37a40f8c4898e915d83dbdfe86f
zaripovasb <sabina.zaripova@nu.edu.kz>
2020-07-13 18:14:29 +0600

    Added navigation to login page

---
+++ app/src/main/java/kz/company/messenger/LoginActvity.kt
@@ -0,0 +1,11 @@
+package kz.company.messenger
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActvity:AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/kz/company/messenger/MainActivity.kt
+++ app/src/main/java/kz/company/messenger/MainActivity.kt
@@ -1,11 +1,21 @@
 package kz.company.messenger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        already_have_an_account_text_view.setOnClickListener{
+            val intent = Intent(this, LoginActvity::class.java)
+            startActivity(intent)
+            Log.d("MainActivity", "Try to navigate")
+        }
     }
+
 }
