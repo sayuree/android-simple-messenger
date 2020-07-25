f2011fb3db672a94587cefa1bf362daba4a458e5
zaripovasb <sabina.zaripova@nu.edu.kz>
2020-07-25 18:21:54 +0600

    Add RecyclerView and UserItem using Groupie library

--- app/src/main/java/kz/company/messenger/NewMessageActivity.kt
+++ app/src/main/java/kz/company/messenger/NewMessageActivity.kt
@@ -2,6 +2,14 @@ package kz.company.messenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+import kotlinx.android.synthetic.main.activity_new_message.*
 
 class NewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +17,22 @@ class NewMessageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_new_message)
 
         supportActionBar?.title = "Select User"
+        val adapter = GroupAdapter<GroupieViewHolder>()
+        recyclerView.adapter = adapter
+        adapter.add(UserItem())
+        fetchUserData()
+    }
+
+    private fun fetchUserData(){
+        val ref  = FirebaseDatabase.getInstance().getReference("/users")
+    }
+}
+
+class UserItem: Item<GroupieViewHolder>(){
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+
+    }
+    override fun getLayout(): Int {
+        return R.layout.user_row_new_message
     }
 }
